e059aab758f9c18a15c0a9a16a66dee65dcae32a
moiwake <miho.kj.sp.195@gmail.com>
2022-08-03 15:41:43 +0900

    Userモデルのvalidationを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,9 @@ class User < ApplicationRecord
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX,
-    message: 'には英字と数字の両方を含めて設定してください', if: :password_required?
+                                 message: 'パスワードには英字と数字の両方を含めて設定してください', if: :password_required?
 
-  validates :user_name, presence: true
-  validates :email, presence: true
+  validates :user_name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 6 }
-  validates :password, presence: true, format: { with: PASSWORD_REGEX }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_01_075427) do
+ActiveRecord::Schema.define(version: 2022_08_02_072946) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2022_08_01_075427) do
     t.string "avator"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
