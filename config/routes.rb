1374bb28a8b57e0a25903e5f2acd71b2ab97829f
moiwake <miho.kj.sp.195@gmail.com>
2022-08-05 16:54:28 +0900

    deviseのルーティング設定修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_scope :user do
+    get 'login', to: 'users/sessions#new', as: :new_user_session
+    post 'login', to: 'users/sessions#create', as: :user_session
+    delete 'logout', to: 'users/sessions#destroy', as: :destroy_user_session
+    get 'signup', to: 'users/registrations#new', as: :new_user_registration
+    post 'signup', to: 'users/registrations#create', as: :user_registration
+    get 'password', to: 'users/passwords#new', as: :new_user_password
+  end
+
   root 'pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
