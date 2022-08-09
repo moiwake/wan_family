51f1a6762836224d6f0cd65bcbdeb7830b37521e
moiwake <miho.kj.sp.195@gmail.com>
2022-08-09 10:20:37 +0900

    ルーティング修正:devise_scopeでエラーとなるため

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  devise_scope :user do
-    get 'login', to: 'users/sessions#new', as: :new_user_session
-    post 'login', to: 'users/sessions#create', as: :user_session
-    delete 'logout', to: 'users/sessions#destroy', as: :destroy_user_session
-    get 'signup', to: 'users/registrations#new', as: :new_user_registration
-    post 'signup', to: 'users/registrations#create', as: :user_registration
-    get 'password', to: 'users/passwords#new', as: :new_user_password
-  end
+  devise_for :users, path_names: {
+    sign_in: "login",
+    sign_out: "logout",
+  }
 
   root 'pages#index'
-    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
