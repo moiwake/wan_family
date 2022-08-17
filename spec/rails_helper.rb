ac7a138c1dc4e67b1f59264d16084c2c658911cb
moiwake <miho.kj.sp.195@gmail.com>
2022-08-17 12:10:40 +0900

    capybara：jsテストの設定

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,7 @@ require_relative '../config/environment'
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'support/capybara'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -30,6 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -68,8 +70,10 @@ RSpec.configure do |config|
     driven_by Capybara.default_driver
   end
 
-  # config.before(:each, type: :system, js: true) do
-  #   driven_by Capybara.javascript_driver
-  #   host! "http://#{Capybara.server_host}:#{Capybara.server_port}"
-  # end
+  config.before(:each, type: :system, js: true) do
+    driven_by Capybara.javascript_driver
+    Capybara.app_host="http://#{Capybara.server_host}"
+  end
+
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
