db2a3b794d75d6e04282cda7bde167519a378366
moiwake <miho.kj.sp.195@gmail.com>
2022-10-12 18:07:46 +0900

    レイアウト修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,12 @@
 class User < ApplicationRecord
   has_one_attached :avatar
 
-  validates :user_name, presence: true, uniqueness: true
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+	validates :user_name, presence: true, uniqueness: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX,
                                  message: 'は英字と数字の両方を含めて設定してください', if: :password_required?,
