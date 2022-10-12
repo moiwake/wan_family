958c9343d993b47b37949c79e2d50553dbd2e5ea
moiwake <miho.kj.sp.195@gmail.com>
2022-10-12 18:05:08 +0900

    adminモデルにタイムアウト機能を追加、バリデーションを追加

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,10 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable, :registerable, :recoverable,
-  devise :database_authenticatable, :rememberable, :validatable
+  # :confirmable, :lockable, :trackable and :omniauthable, :registerable, :recoverable,
+  devise :database_authenticatable, :rememberable, :validatable, :timeoutable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX,
+                                 message: 'は英字と数字の両方を含めて設定してください', if: :password_required?,
+                                 allow_blank: true
 end
