7d62b5a1f1160e1dfb9ff36f7049edbb9f7c60b1
moiwake <miho.kj.sp.195@gmail.com>
2022-10-17 22:29:23 +0900

    rspecのcapybaraの設定を修正：エラー解消のためオプションを追加

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,14 +1,32 @@
-Capybara.default_driver    = :rack_test
-Capybara.javascript_driver = :selenium_chrome
+require 'webdrivers/chromedriver'
+
+Capybara.default_driver = :rack_test
+Capybara.javascript_driver = :remote_chrome
 Capybara.server_host = "web"
 Capybara.server_port = 3001
 Capybara.default_max_wait_time = 5
 Capybara.ignore_hidden_elements = true
-Capybara.register_driver :selenium_chrome do |app|
-  opts = {
-    capabilities: :chrome,
-    browser: :remote,
-    url: "http://chrome:4444/wd/hub",
+
+Capybara.register_driver :remote_chrome do |app|
+  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome
+  capabilities['goog:chromeOptions'] = {
+    args: [
+      "--no-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-popup-blocking",
+      "--disable-gpu",
+      "--headless",
+      "--window-size=1920,1080",
+    ]
   }
-  Capybara::Selenium::Driver.new(app, opts)
+
+  url = "http://chrome:4444/wd/hub"
+
+  driver_opts = {
+    browser: :remote,
+    capabilities: capabilities,
+    url: url,
+  }
+
+  Capybara::Selenium::Driver.new app, **driver_opts
 end
