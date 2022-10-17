14792928310e0525b8d895488b9858351f5b8dd7
moiwake <miho.kj.sp.195@gmail.com>
2022-10-17 21:46:24 +0900

    ログインユーザーによってトップページの表示を変える：トップページのレイアウト変更、ログインユーザーの判定メソッドを定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def who_signed_in?
+    if current_user.present?
+      return "user"
+    elsif current_admin.present?
+      return "admin"
+    end
+    return nil
+  end
 end
