5b9aaeaee6d1cf192199760047b43d76e5a3a4fd
moiwake <miho.kj.sp.195@gmail.com>
2022-10-20 13:27:28 +0900

    rspec：userのテストを修正：他ユーザーへのアクセス不可のテストをシステムからリクエストへ

--- spec/factories/admins_factory_bot.rb
+++ spec/factories/admins_factory_bot.rb
@@ -4,10 +4,4 @@ FactoryBot.define do
     password { "adminpass00" }
     password_confirmation { "adminpass00" }
   end
-
-  factory :another_admin, class: "Admin" do
-    email { "another@email.com" }
-    password { "anotherpass01" }
-    password_confirmation { "anotherpass01" }
-  end
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -2,8 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   let!(:user) { create(:user, :updated_profile_user) }
+  let!(:another_user) { create(:user, :updated_profile_user, introduction: "another_introduction") }
   let(:avatar_url) do
     user.avatar.url.split('/').last
+  end
+  let(:another_avatar_url) do
+    another_user.avatar.attach(io: File.open('spec/fixtures/images/test2.png'), filename: 'test2.jpeg', content_type: 'image/png')
+    another_user.avatar.url.split('/').last
   end
 
   describe "GET /index" do
@@ -18,9 +23,14 @@ RSpec.describe "Users", type: :request do
         expect(response).to have_http_status(:success)
       end
 
-      it "ユーザー情報が表示される" do
+      it "ログインユーザー情報が表示される" do
         expect(response.body).to include(user.name)
         expect(response.body).to include(avatar_url)
+      end
+
+      it "ログインユーザー以外の情報は表示されない" do
+        expect(response.body).not_to include(another_user.name)
+        expect(response.body).not_to include(another_avatar_url)
       end
     end
 
@@ -44,10 +54,16 @@ RSpec.describe "Users", type: :request do
         expect(response).to have_http_status(:success)
       end
 
-      it "ユーザー情報が表示される" do
+      it "ログインユーザー情報が表示される" do
         expect(response.body).to include(user.name)
         expect(response.body).to include(avatar_url)
         expect(response.body).to include(user.introduction)
+      end
+
+      it "ログインユーザー以外の情報は表示されない" do
+        expect(response.body).not_to include(another_user.name)
+        expect(response.body).not_to include(another_avatar_url)
+        expect(response.body).not_to include(another_user.introduction)
       end
     end
 
@@ -60,11 +76,9 @@ RSpec.describe "Users", type: :request do
   end
 
   describe "PATCH /update" do
-    before do
-      sign_in user
-    end
-
     context "ログインしているとき" do
+      let(:user) { create(:user) }
+      let(:another_user) { create(:user) }
       let(:update_attributes) do
         {
           introduction: "更新しました。",
@@ -72,24 +86,33 @@ RSpec.describe "Users", type: :request do
         }
       end
       let(:update_avatar) do
-        fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'test.jpeg'), 'image/jpeg')
+        fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'test1.png'), 'image/png')
       end
       let(:update_avatar_url) do
         user.avatar.url.split('/').last
       end
 
-      it "usersテーブルのintroductionカラム、avatarカラムを更新できる" do
+      before do
+        sign_in user
         patch profile_path, params: { user: update_attributes }
         ActiveStorage::Current.host = "http://www.example.com"
+      end
 
+      it "ログインユーザーのintroductionカラム、avatarカラムを更新できる" do
         expect do
           expect(user.introduction).to eq(update_attributes[:introduction])
           expect(user.avatar.url).to include(update_avatar_url)
         end.to change { User.count }.by(0)
       end
 
-      it "更新後にリダイレクトする" do
-        patch profile_path, params: { user: update_attributes }
+      it "ログインユーザー以外のintroductionカラム、avatarカラムは更新されない" do
+        expect do
+          expect(another_user.introduction).not_to eq(update_attributes[:introduction])
+          expect(another_user.avatar.url).to eq(nil)
+        end.to change { User.count }.by(0)
+      end
+
+      it "更新に成功すればリダイレクトする" do
         expect(response).to redirect_to(edit_profile_path)
       end
     end
