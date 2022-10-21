4720d6faec087442ee58d6a3304269d09a7d5dc0
moiwake <miho.kj.sp.195@gmail.com>
2022-10-21 12:55:25 +0900

    アプリケーションヘルパーのテストを作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,4 +77,5 @@ RSpec.configure do |config|
 
   config.include Devise::Test::IntegrationHelpers, type: :request
   config.include Devise::Test::IntegrationHelpers, type: :system
+  config.include Devise::Test::IntegrationHelpers, type: :helper
 end
