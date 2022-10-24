9f04046b76be2b4c249cb35fd2c6c14fb5f93216
moiwake <miho.kj.sp.195@gmail.com>
2022-10-24 19:12:17 +0900

    spotsのバリデーションをモデルに追加

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,2 +1,10 @@
 class Spot < ApplicationRecord
+  with_options presence: true do
+    validates :name
+    validates :latitude, uniqueness: { scope: :longitude }
+    validates :longitude
+    validates :address, uniqueness: true
+    validates :category
+    validates :allowed_area
+  end
 end
