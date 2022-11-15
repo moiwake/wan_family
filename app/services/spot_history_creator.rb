f14fad4117e7b5d0f27f8796dd216808a521d15d
moiwake <miho.kj.sp.195@gmail.com>
2022-11-15 09:49:30 +0900

    新規登録と更新の条件分岐が機能していないため修正

--- app/services/spot_history_creator.rb
+++ app/services/spot_history_creator.rb
@@ -1,26 +1,17 @@
 class SpotHistoryCreator
-  attr_accessor :spot, :user
+  attr_accessor :spot, :user, :history
 
-  def self.call(spot: nil, user: nil)
-    new(spot: spot, user: user).call_create_spot_histories
+  def self.call(spot: nil, user: nil, history: nil)
+    new(spot: spot, user: user, history: history).create_spot_histories
   end
 
-  def initialize(spot: nil, user: nil)
+  def initialize(spot: nil, user: nil, history: nil)
     @spot = spot
     @user = user
+    @history = history
   end
 
-  def call_create_spot_histories
-    if spot.new_record?
-      create_spot_histories("新規登録")
-    elsif spot.persisted?
-      create_spot_histories("更新")
-    end
-  end
-
-  private
-
-  def create_spot_histories(history)
+  def create_spot_histories
     spot.spot_histories.create(user_id: user.id, history: history)
   end
 end
