3cbf0faff6a2dc03865e8dbd468238f9ff13b662
moiwake <miho.kj.sp.195@gmail.com>
2022-11-16 11:09:10 +0900

    ヘルパーの修正：ary_present_and_include_ele?を定義、外部との依存関係を解消

--- app/helpers/spots_helper.rb
+++ app/helpers/spots_helper.rb
@@ -1,9 +1,7 @@
 module SpotsHelper
-  def rule_opt_is_attached?(attached_rule_opt_ids: nil, rule_opt_id: nil)
-    if attached_rule_opt_ids.present?
-      if attached_rule_opt_ids.include?(rule_opt_id)
-        true
-      end
+  def ary_present_and_include_ele?(ary: nil, ele: nil)
+    if ary.present?
+      ary.include?(ele)
     else
       false
     end
