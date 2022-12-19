2d2fbdb5eb9eebbb4a75a8100205e4b9d303a307
moiwake <miho.kj.sp.195@gmail.com>
2022-12-19 17:20:47 +0900

    set_spotの追加：記述の整理、includesの追加：N+1対処

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,12 @@
 class ReviewsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_spot
 
   def index
-    @spot = Spot.find(params[:spot_id])
-    @reviews = Review.where(spot_id: params[:spot_id])
+    @reviews = Review.where(spot_id: params[:spot_id]).includes_image.includes(:user)
   end
 
   def new
-    @spot = Spot.find(params[:spot_id])
     @review_poster_form = ReviewPosterForm.new
   end
 
@@ -16,38 +15,37 @@ class ReviewsController < ApplicationController
 
     if @review_poster_form.save
       flash[:notice] = "新規のレビューを投稿しました。"
-      redirect_to spot_review_path(@review_poster_form.review.spot, @review_poster_form.review)
+      redirect_to spot_review_path(@spot, @review_poster_form.review)
     else
-      @spot = Spot.find(params[:spot_id])
       render "new"
     end
   end
 
   def show
-    @review = Review.find(params[:id])
+    @review = Review.includes_image.find(params[:id])
   end
 
   def edit
-    @review = Review.find(params[:id])
+    @review = Review.includes_image.find(params[:id])
     @review_poster_form = ReviewPosterForm.new(review: @review, image: @review.image)
   end
 
   def update
-    @review = Review.find(params[:id])
+    @review = Review.includes_image.find(params[:id])
     @review_poster_form = ReviewPosterForm.new(attributes: form_params,review: @review, image: @review.image)
 
     if @review_poster_form.save && delete_image_file
-      flash[:notice] = "#{@review_poster_form.review.spot.name}のレビューを変更しました。"
-      redirect_to spot_review_path(@review_poster_form.review.spot, @review_poster_form.review)
+      flash[:notice] = "#{@spot.name}のレビューを変更しました。"
+      redirect_to spot_review_path(@spot, @review_poster_form.review)
     else
       render "edit"
     end
   end
 
   def destroy
-    @review = Review.find(params[:id])
+    @review = Review.includes_image.includes(:spot).find(params[:id])
     @review.destroy
-    flash[:notice] = "#{@review.title}のレビューを削除しました。"
+    flash[:notice] = "#{@review.spot.name}のレビュー「#{@review.title}」を削除しました。"
     redirect_to users_review_index_path
   end
 
@@ -58,12 +56,17 @@ class ReviewsController < ApplicationController
     params.require(:review_poster_form).permit(review_attributes: review_params, image_attributes: [:user_id, :spot_id, :review_id, files: []])
   end
 
+  def set_spot
+    @spot = Spot.find(params[:spot_id])
+  end
+
   def delete_image_file
-    files_blob_ids_params = params[:review_poster_form][:image_attributes][:files_blob_ids]
-    if files_blob_ids_params
+    if params[:review_poster_form][:image_attributes]
+      files_blob_ids_params = params[:review_poster_form][:image_attributes][:files_blob_ids]
+
       files_blob_ids_params.each do |file_id|
-        delete_file = @review.image.files.find { |file| file.id == file_id.to_i }
-        delete_file.purge
+        removed_file = @review.image.files.find { |file| file.id == file_id.to_i }
+        removed_file.purge
       end
     end
 
