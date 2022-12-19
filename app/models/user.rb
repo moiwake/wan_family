cba5e376cb24876c98e8cabd3f4c5c8a226e7188
moiwake <miho.kj.sp.195@gmail.com>
2022-12-19 17:27:59 +0900

    ユーザー画像のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  has_many         :spot_histories, dependent: :destroy
-  has_many         :spots, through: :spot_histories
-  has_many         :reviews, dependent: :destroy
-  has_many         :images, dependent: :destroy
+  has_many :spot_histories, dependent: :destroy
+  has_many :spots, through: :spot_histories
+  has_many :reviews, dependent: :destroy
+  has_many :images, dependent: :destroy
 
   has_one_attached :avatar
 
@@ -12,8 +12,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true, uniqueness: true
+  validates :avatar, blob: {
+    content_type: ['image/png', 'image/jpg', 'image/jpeg'],
+    size_range: 1..(5.megabytes),
+  }, on: :update
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX,
-                                 message: 'は英字と数字の両方を含めて設定してください', if: :password_required?,
-                                 allow_blank: true
+  validates :password, format: {
+    with: PASSWORD_REGEX,
+    message: 'は英字と数字の両方を含めて設定してください', if: :password_required?,
+    allow_blank: true
+  }
 end
