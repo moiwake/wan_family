49d62b15021043e27841e09c34d8d038b6a0df96
moiwake <miho.kj.sp.195@gmail.com>
2022-12-19 17:57:06 +0900

    エラーの解消、テスト方法の見直し：属性値の設定のテストを追加、追加によって不要になったテストを整理

--- spec/forms/spot_register_form_spec.rb
+++ spec/forms/spot_register_form_spec.rb
@@ -1,19 +1,80 @@
 require 'rails_helper'
 
 RSpec.describe SpotRegisterForm, type: :model do
-  let!(:new_spot) { Spot.new }
+  let(:categories) { create_list(:category, 2) }
+  let(:allowed_areas) { create_list(:allowed_area, 2) }
+  let(:rule_options) { RuleOption.all.limit(4) }
+
   let!(:saved_spot) { create(:spot, :with_rules) }
   let!(:saved_rules) { saved_spot.rule }
 
-  let(:spot_attributes) { FactoryBot.attributes_for(:spot, allowed_area_id: AllowedArea.first.id, category_id: Category.first.id) }
-  let(:rules_attributes) do
-    keys = RuleOption.pluck(:id).map(&:to_s)
-    values = rules_attributes_values
-    keys.zip(values).to_h
+  let(:params) { { spot_attributes: spot_params, rules_attributes: rules_params } }
+  let(:spot_params) { FactoryBot.attributes_for(:spot, allowed_area_id: allowed_areas.first.id, category_id: categories.first.id) }
+  let(:rules_params) do
+    {
+      "#{rule_options[0].id}" => { "answer" => "1" },
+      "#{rule_options[1].id}" => { "answer" => "1" },
+      "#{rule_options[2].id}" => { "answer" => "0" },
+      "#{rule_options[3].id}" => { "answer" => "0" },
+    }
   end
-  let(:rules_attributes_values) do
-    RuleOption.pluck(:id).map do |id|
-      FactoryBot.attributes_for(:rule, rule_option_id: id)
+
+  let(:updated_params) { { spot_attributes: updated_spot_params, rules_attributes: updated_rules_params } }
+  let(:updated_spot_params) { FactoryBot.attributes_for(:spot, allowed_area_id: allowed_areas.last.id, category_id: categories.last.id) }
+  let(:updated_rules_params) do
+    {
+      "#{rule_options[0].id}" => { "answer" => "0" },
+      "#{rule_options[1].id}" => { "answer" => "0" },
+      "#{rule_options[2].id}" => { "answer" => "1" },
+      "#{rule_options[3].id}" => { "answer" => "1" },
+    }
+  end
+
+  let(:invalid_params) { { spot_attributes: invalid_spot_params, rules_attributes: { nil => {} } } }
+  let(:invalid_spot_params) { FactoryBot.attributes_for(:spot, :invalid_spot) }
+
+  describe "#spot_attributes=" do
+    let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: params) }
+    let(:spot) { spot_register_form_instance.spot }
+
+    it "パラメータの値をスポットインスタンスの属性値に設定する" do
+      expect(spot.name).to eq(spot_params[:name])
+      expect(spot.latitude).to eq(spot_params[:latitude])
+      expect(spot.longitude).to eq(spot_params[:longitude])
+      expect(spot.address).to eq(spot_params[:address])
+      expect(spot.official_site).to eq(spot_params[:official_site])
+      expect(spot.allowed_area_id).to eq(spot_params[:allowed_area_id])
+      expect(spot.category_id).to eq(spot_params[:category_id])
+    end
+  end
+
+  describe "#rules_attributes=" do
+    context "spot_register_form_instanceのrulesオブジェクトがnilのとき" do
+      let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: params) }
+      let(:rules) { spot_register_form_instance.spot.rule }
+
+      it "パラメータのキーの数だけ、同伴ルールのレコードを作成する" do
+        expect(rules.length).to eq(rules_params.keys.length)
+      end
+
+      it "作成した同伴ルールレコードの属性値に、パラメータの値と指定の外部キーを設定する" do
+        rules_params.each.with_index do |(key, value), i|
+          expect(rules[i].answer).to eq(value["answer"])
+          expect(rules[i].rule_option_id.to_s).to eq(key)
+        end
+      end
+    end
+
+    context "spot_register_form_instanceのrulesオブジェクトがnilではないとき" do
+      let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: updated_params, spot: saved_spot, rules: saved_rules) }
+      let(:rules) { spot_register_form_instance.rules }
+
+      it "パラメータの値を同伴ルールレコードの属性値に設定する" do
+        updated_rules_params.each.with_index do |(key, value), i|
+          expect(rules[i].answer).to eq(value["answer"])
+          expect(rules[i].rule_option_id.to_s).to eq(key)
+        end
+      end
     end
   end
 
@@ -22,32 +83,15 @@ RSpec.describe SpotRegisterForm, type: :model do
 
     context "スポット、同伴ルールの属性値が妥当な場合" do
       context "新規登録" do
-        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: attributes, spot: new_spot) }
-        let(:attributes) { { spot_attributes: spot_attributes, rules_attributes: rules_attributes } }
+        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: params) }
+        let(:params) { { spot_attributes: spot_params, rules_attributes: rules_params } }
 
-        it "パラメータの値を属性値に設定して、スポットのレコードが保存される" do
-          expect do
-            subject
-          end.to change { Spot.count }.by(1)
-
-          expect(new_spot.name).to eq(spot_attributes[:name])
-          expect(new_spot.latitude).to eq(spot_attributes[:latitude])
-          expect(new_spot.longitude).to eq(spot_attributes[:longitude])
-          expect(new_spot.address).to eq(spot_attributes[:address])
-          expect(new_spot.official_site).to eq(spot_attributes[:official_site])
-          expect(new_spot.allowed_area_id).to eq(spot_attributes[:allowed_area_id])
-          expect(new_spot.category_id).to eq(spot_attributes[:category_id])
+        it "スポットレコードが保存される" do
+          expect { subject }.to change { Spot.count }.by(1)
         end
 
-        it "パラメータの値を属性値に設定して、同伴ルールのすべてのレコードが保存される" do
-          expect do
-            subject
-          end.to change { Rule.count }.by(spot_register_form_instance.spot.rule.length)
-
-          new_spot.rule.each do |new_rule|
-            expect(new_rule.answer).to eq(rules_attributes[new_rule.rule_option_id.to_s][:answer])
-            expect(new_rule.rule_option_id).to eq(rules_attributes[new_rule.rule_option_id.to_s][:rule_option_id])
-          end
+        it "同伴ルールのすべてのレコードが保存される" do
+          expect { subject }.to change { Rule.count }.by(spot_register_form_instance.spot.rule.length)
         end
 
         it "trueを返す" do
@@ -56,30 +100,27 @@ RSpec.describe SpotRegisterForm, type: :model do
       end
 
       context "更新" do
-        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: attributes, spot: saved_spot, rules: saved_rules) }
-        let(:attributes) { { spot_attributes: spot_attributes, rules_attributes: rules_attributes } }
+        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: updated_params, spot: saved_spot, rules: saved_rules) }
 
-        it "パラメータの値を属性値に設定して、スポットのレコードが更新される" do
-          expect do
-            subject
-            expect(saved_spot.name).to eq(spot_attributes[:name])
-            expect(saved_spot.latitude).to eq(spot_attributes[:latitude])
-            expect(saved_spot.longitude).to eq(spot_attributes[:longitude])
-            expect(saved_spot.address).to eq(spot_attributes[:address])
-            expect(saved_spot.official_site).to eq(spot_attributes[:official_site])
-            expect(saved_spot.allowed_area_id).to eq(spot_attributes[:allowed_area_id])
-            expect(saved_spot.category_id).to eq(spot_attributes[:category_id])
-          end.to change { Spot.count }.by(0)
+        it "スポットレコードが更新される" do
+          saved_spot.reload
+          expect { subject }.to change { Spot.count }.by(0)
+          expect(saved_spot.saved_change_to_name?).to eq(true)
+          expect(saved_spot.saved_change_to_latitude?).to eq(true)
+          expect(saved_spot.saved_change_to_longitude?).to eq(true)
+          expect(saved_spot.saved_change_to_address?).to eq(true)
+          expect(saved_spot.saved_change_to_official_site?).to eq(true)
+          expect(saved_spot.saved_change_to_allowed_area_id?).to eq(true)
+          expect(saved_spot.saved_change_to_category_id?).to eq(true)
         end
 
-        it "同伴ルールのすべてレコードが更新される" do
-          expect do
-            subject
-            saved_rules.each do |saved_rule|
-              expect(saved_rule.answer).to eq(rules_attributes[saved_rule.rule_option_id.to_s][:answer])
-              expect(saved_rule.rule_option_id).to eq(rules_attributes[saved_rule.rule_option_id.to_s][:rule_option_id])
-            end
-          end.to change { Rule.count }.by(0)
+        it "同伴ルールのすべてのレコードが更新される" do
+          saved_rules.each(&:reload)
+          expect { subject }.to change { Rule.count }.by(0)
+
+          saved_rules.each do |saved_rule|
+            expect(saved_rule.saved_change_to_answer?).to eq(true)
+          end
         end
 
         it "trueを返す" do
@@ -89,22 +130,15 @@ RSpec.describe SpotRegisterForm, type: :model do
     end
 
     context "スポット、同伴ルールの属性値が不正な場合" do
-      let(:invalid_attributes) { { spot_attributes: invalid_spot_attributes, rules_attributes: { nil => {} } } }
-      let(:invalid_spot_attributes) { FactoryBot.attributes_for(:spot, :invalid_spot) }
-
       context "新規登録" do
-        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: invalid_attributes) }
+        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: invalid_params) }
 
         it "スポットのレコードは保存されない" do
-          expect do
-            subject
-          end.to change { Spot.count }.by(0)
+          expect { subject }.to change { Spot.count }.by(0)
         end
 
         it "同伴ルールのすべてのレコードが保存されない" do
-          expect do
-            subject
-          end.to change { Rule.count }.by(0)
+          expect { subject }.to change { Rule.count }.by(0)
         end
 
         it "falseを返す" do
@@ -113,32 +147,30 @@ RSpec.describe SpotRegisterForm, type: :model do
       end
 
       context "更新" do
-        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: invalid_attributes, spot: saved_spot, rules: saved_rules) }
+        let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: invalid_params, spot: saved_spot, rules: saved_rules) }
 
         it "スポットのレコードは更新されない" do
-          expect do
-            subject
-            expect(saved_spot.reload.name_changed?).to eq(false)
-            expect(saved_spot.address_changed?).to eq(false)
-            expect(saved_spot.latitude_changed?).to eq(false)
-            expect(saved_spot.longitude_changed?).to eq(false)
-            expect(saved_spot.allowed_area_id_changed?).to eq(false)
-            expect(saved_spot.category_id_changed?).to eq(false)
-            expect(saved_spot.official_site_changed?).to eq(false)
-          end.to change { Rule.count }.by(0)
+          saved_spot.reload
+          expect { subject }.to change { Spot.count }.by(0)
+          expect(saved_spot.reload.name_changed?).to eq(false)
+          expect(saved_spot.address_changed?).to eq(false)
+          expect(saved_spot.latitude_changed?).to eq(false)
+          expect(saved_spot.longitude_changed?).to eq(false)
+          expect(saved_spot.allowed_area_id_changed?).to eq(false)
+          expect(saved_spot.category_id_changed?).to eq(false)
+          expect(saved_spot.official_site_changed?).to eq(false)
         end
 
         it "同伴ルールのすべてのレコードが更新されない" do
-          expect do
-            subject
-            saved_rules.each do |saved_rule|
-              expect(saved_rule.answer_changed?).to eq(false)
-            end
-          end.to change { Rule.count }.by(0)
+          saved_rules.each(&:reload)
+          expect { subject }.to change { Rule.count }.by(0)
+
+          saved_rules.each do |saved_rule|
+            expect(saved_rule.saved_change_to_answer?).to eq(false)
+          end
         end
 
         it "falseを返す" do
-          subject
           expect(subject).to eq(false)
         end
       end
@@ -148,42 +180,38 @@ RSpec.describe SpotRegisterForm, type: :model do
   describe "#check_and_add_error" do
     subject { spot_register_form_instance.send(:check_and_add_error) }
 
-    context "スポット、もしくは同伴ルールの属性値が無効な場合" do
-      let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: invalid_attributes) }
+    context "スポットの属性値が無効な場合" do
+      let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: invalid_params) }
+      let(:invalid_params) { { spot_attributes: invalid_spot_params, rules_attributes: rules_params } }
 
-      context "スポットの属性値が無効な場合" do
-        let(:invalid_attributes) { { spot_attributes: invalid_spot_attributes, rules_attributes: rules_attributes } }
-        let(:invalid_spot_attributes) { FactoryBot.attributes_for(:spot, :invalid_spot) }
-
-        it "スポットの属性に関するエラーメッセージがerrorsコレクションに追加される" do
-          subject
-          expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:name)}を入力してください")
-          expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:address)}を入力してください")
-          expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:category)}を入力してください")
-          expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:allowed_area)}を入力してください")
-        end
-
-        it "addressの属性値にエラーがあるとき、latitude、longitudeのエラーはerrorsコレクションに追加されない" do
-          subject
-          expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:address)}を入力してください")
-          expect(spot_register_form_instance.errors[:base]).not_to include("#{Spot.human_attribute_name(:latitude)}を入力してください")
-          expect(spot_register_form_instance.errors[:base]).not_to include("#{Spot.human_attribute_name(:longitude)}を入力してください")
-        end
+      it "スポットレコードのエラーメッセージがerrorsコレクションに追加される" do
+        subject
+        expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:name)}を入力してください")
+        expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:address)}を入力してください")
+        expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:category)}を入力してください")
+        expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:allowed_area)}を入力してください")
       end
 
-      context "同伴ルールの属性値が無効な場合" do
-        let(:invalid_attributes) { { spot_attributes: spot_attributes, rules_attributes: { nil => {} } } }
-
-        it "同伴ルールの属性に関するエラーメッセージがerrorsコレクションに追加される" do
-          subject
-          expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:rule)}は不正な値です")
-        end
+      it "addressカラムにエラーがあるとき、latitude、longitudeカラムのエラーはerrorsコレクションに追加されない" do
+        subject
+        expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:address)}を入力してください")
+        expect(spot_register_form_instance.errors[:base]).not_to include("#{Spot.human_attribute_name(:latitude)}を入力してください")
+        expect(spot_register_form_instance.errors[:base]).not_to include("#{Spot.human_attribute_name(:longitude)}を入力してください")
       end
     end
 
-    context "スポット、もしくは同伴ルールの属性値が有効な場合" do
-      let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: attributes) }
-      let(:attributes) { { spot_attributes: spot_attributes, rules_attributes: rules_attributes } }
+    context "同伴ルールの属性値が無効な場合" do
+      let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: invalid_params) }
+      let(:invalid_params) { { spot_attributes: spot_params, rules_attributes: { nil => {} } } }
+
+      it "同伴ルールレコードのエラーメッセージがerrorsコレクションに追加される" do
+        subject
+        expect(spot_register_form_instance.errors[:base]).to include("#{Spot.human_attribute_name(:rule)}は不正な値です")
+      end
+    end
+
+    context "スポットと同伴ルールの属性値が有効な場合" do
+      let(:spot_register_form_instance) { SpotRegisterForm.new(attributes: params) }
 
       it "falseを返す" do
         expect(subject).to eq(false)
@@ -191,3 +219,4 @@ RSpec.describe SpotRegisterForm, type: :model do
     end
   end
 end
+
