3e48d39e9e41fd92f5447ab11808dac7da4c9080
moiwake <miho.kj.sp.195@gmail.com>
2022-12-19 16:57:50 +0900

    レビュー評価の表示をJSで実装

--- app/javascript/packs/display_rating.js
+++ app/javascript/packs/display_rating.js
@@ -1,10 +1,10 @@
-displayRating("#dog-ratings", "dog-rating-value");
-displayRating("#human-ratings", "human-rating-value");
+let reviewsLength = document.getElementById("reviews-count");
 
-function displayRating(ratingClass, ratingValueClass) {
-  let reviewsLength = document.getElementById("reviews-count");
+if (reviewsLength) {
+  displayRating("#dog-ratings", "dog-rating-value");
+  displayRating("#human-ratings", "human-rating-value");
 
-  if (reviewsLength) {
+  function displayRating(ratingClass, ratingValueClass) {
     for (let i = 0; i < reviewsLength.value; i++) {
       let reviewId = document.getElementById("review-id" + i).value
       let rating = document.querySelector(ratingClass + reviewId).children;
@@ -15,5 +15,18 @@ function displayRating(ratingClass, ratingValueClass) {
         rating[j].classList.add("colored");
       }
     }
-  }
-};
+  };
+} else if (!reviewsLength) {
+  displayRating(".dog-ratings", "dog-rating-value");
+  displayRating(".human-ratings", "human-rating-value");
+
+  function displayRating(ratingClass, ratingValueClass) {
+    let rating = document.querySelector(ratingClass).children;
+    let ratingValue = document.getElementById(ratingValueClass);
+
+    for (let j = 0; j < ratingValue.value; j++){
+      rating[j].classList.remove("non-colored");
+      rating[j].classList.add("colored");
+    }
+  };
+}
