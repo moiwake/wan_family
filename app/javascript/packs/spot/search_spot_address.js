7a0de50cc9933ad825bf9824a32f12115f01bf7c
moiwake <miho.kj.sp.195@gmail.com>
2023-01-02 12:53:28 +0900

    変数名の変更：役割を明確化

--- app/javascript/packs/spot/search_spot_address.js
+++ app/javascript/packs/spot/search_spot_address.js
@@ -1,23 +1,18 @@
-function docInit(){
-  if (aft == true){
-    document.getElementById("form_spot_lat").value = "";
-    document.getElementById("form_spot_lng").value = "";
-    document.getElementById("form_spot_address").value = "";
-    marker.setMap(null);
-  }
+let search_button = document.getElementById("form_search_button");
+if( search_button !== null ) {
+  search_button.addEventListener("click", searchAddress);
 }
 
 let aft
-function codeAddress(){
-  let inputAddress = document.getElementById("form_search_spot").value;
+function searchAddress(){
+  let searchWord = document.getElementById("form_search_spot").value;
 
   geocoder = new google.maps.Geocoder()
-  geocoder.geocode( { "address": inputAddress }, function(results, status) {
+  geocoder.geocode( { "address": searchWord }, function(results, status) {
     if (status == "OK") {
       let addressArray = results[0].formatted_address.split(" ");
 
       if (addressArray[1]){
-
         document.getElementById("form_spot_lat").value = results[0].geometry.location.lat();
         document.getElementById("form_spot_lng").value = results[0].geometry.location.lng();
         document.getElementById("form_spot_address").value = addressArray[1];
@@ -25,20 +20,23 @@ function codeAddress(){
         aft = true;
       } else{
         alert("スポットを特定できませんでした。検索ワードを変更してください。");
-        docInit();
       }
     } else {
-      if(!inputAddress){
+      if(!searchWord){
         alert("検索ワードを入力してください。");
       } else {
         alert("該当するスポットがありませんでした。");
       }
-      docInit();
     }
   });
+
+  resetAddress();
 }
 
-let search_button = document.getElementById("form_search_button");
-if( search_button !== null ) {
-  search_button.addEventListener("click", codeAddress);
+function resetAddress(){
+  if (aft == true){
+    document.getElementById("form_spot_lat").value = "";
+    document.getElementById("form_spot_lng").value = "";
+    document.getElementById("form_spot_address").value = "";
+  }
 }
