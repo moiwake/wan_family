47242003dc88832605caeea48f6e36d976d402ac
moiwake <miho.kj.sp.195@gmail.com>
2023-01-23 13:16:10 +0900

    like_reviewsコントローラー：reviewレコードの取得方法を変更：クエリ発行数を減らすため

--- app/controllers/like_reviews_controller.rb
+++ app/controllers/like_reviews_controller.rb
@@ -1,14 +1,15 @@
 class LikeReviewsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_review
 
   def create
     @like_review = current_user.like_reviews.create(review_id: params[:review_id])
+    set_review
     render "create_and_destroy"
   end
 
   def destroy
     @like_review = LikeReview.find(params[:id])
+    set_review
     @like_review.destroy
     render "create_and_destroy"
   end
@@ -16,6 +17,6 @@ class LikeReviewsController < ApplicationController
   private
 
   def set_review
-    @review = Review.find(params[:review_id])
+    @review = @like_review.review
   end
 end
