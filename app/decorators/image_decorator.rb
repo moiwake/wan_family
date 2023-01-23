0442710e56ddc18146d78af242b2bbe69cb6d57a
moiwake <miho.kj.sp.195@gmail.com>
2023-01-23 22:07:57 +0900

    レビュー編集ページ：画像削除フォームのパーシャルをcollectionオプションで記述、get_files_for_editヘルパーメソッド追加：画像データを降順で取得

--- app/decorators/image_decorator.rb
+++
@@ -1,7 +0,0 @@
-class ImageDecorator < Draper::Decorator
-  delegate_all
-
-  def reject_invalid_files
-    object.default_files.reject { |file| file.id == nil }
-  end
-end
--- app/helpers/review_helper.rb
+++ app/helpers/review_helper.rb
@@ -1,3 +1,13 @@
 module ReviewHelper
+  def get_files_for_edit(object)
+    if object.present? && object.image.present?
+      set_default_files(object.image.reload.files)
+    else
+      nil
+    end
+  end
 
+  def set_default_files(files)
+    files.eager_load(:blob).order("blob.created_at desc, blob.id desc")
+  end
 end
