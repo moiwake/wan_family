54b7a4e2abdcbd3bc5b3172779c872851795f503
moiwake <miho.kj.sp.195@gmail.com>
2023-01-23 21:55:21 +0900

    ReviewsForSpotQuery：parent_recordをcallメソッドの引数に：reviewレコードを親レコードで絞れるように

--- app/queries/reviews_for_spot_query.rb
+++ app/queries/reviews_for_spot_query.rb
@@ -1,14 +1,37 @@
 class ReviewsForSpotQuery < QueryBase
   class << self
-    def call(reviews: Review.all, search_condition_hash: {}, params: {})
-      scope = set_default_scope(reviews).where(search_condition_hash)
+    def call(reviews: Review.all, parent_record: nil, params: {})
+      scope = set_default_scope(reviews)
+      scope = search_by_parent_record(scope, parent_record)
       order_scope(scope, params)
     end
 
     private
 
     def set_default_scope(reviews)
-      reviews.includes_image.includes(:user, :spot, :like_reviews)
+      scope = eager_load_in_scope(reviews)
+      scope = preload_in_scope(scope)
+      scope = order_blob_record(scope)
+    end
+
+    def eager_load_in_scope(scope)
+      scope.eager_load(:user, :spot, image: { files_attachments: { blob: :variant_records } })
+    end
+
+    def preload_in_scope(scope)
+      scope.preload(:like_reviews)
+    end
+
+    def order_blob_record(scope)
+      scope.order("blob.created_at desc, blob.id desc")
+    end
+
+    def search_by_parent_record(scope, parent_record)
+      if parent_record.is_a?(User)
+        scope.where(user_id: parent_record.id)
+      elsif parent_record.is_a?(Spot)
+        scope.where(spot_id: parent_record.id)
+      end
     end
 
     def set_ids_in_order_likes
