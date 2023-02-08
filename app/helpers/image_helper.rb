a3b1c983dad4331ba663a5d4aef4c46bf51d98cd
moiwake <miho.kj.sp.195@gmail.com>
2023-02-08 10:47:28 +0900

    image_helper：image_link_params_hash、get_parent_imageメソッド：ヘルパースペックテスト作成、不要なコードを削除

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,20 +1,10 @@
 module ImageHelper
-  def image_link_params_hash(blob:)
+  def image_link_params_hash(blob)
     params_hash = {
       spot_id: get_parent_image(blob).spot_id,
       id: get_parent_image(blob).id,
       blob_id: blob.id,
     }
-
-    if params[:by] == "created_at" && params[:direction] == "desc"
-      return params_hash.merge({ by: "created_at", direction: "desc" })
-    elsif params[:by] == "created_at" && params[:direction] == "asc"
-      return params_hash.merge({ by: "created_at", direction: "asc" })
-    elsif params[:by] == "likes_count" && params[:direction] == "desc"
-      return params_hash.merge({ by: "likes_count", direction: "desc" })
-    else
-      return params_hash
-    end
   end
 
   def get_parent_image(blob)
