f18a5dd5137b76c7934ff9d219becaee7804dc38
moiwake <miho.kj.sp.195@gmail.com>
2023-02-08 12:24:29 +0900

    spot_tag_helper：tagged?、exceed_max?、difference_of_maxメソッド：ヘルパースペックテスト作成

--- app/helpers/spot_tag_helper.rb
+++ app/helpers/spot_tag_helper.rb
@@ -1,13 +1,13 @@
 module SpotTagHelper
-  def tagged?(spot:)
+  def tagged?(spot)
     current_user.spot_tags.exists?(spot_id: spot.id)
   end
 
-  def exceed_max?(spot_tags:)
+  def exceed_max?(spot_tags)
     spot_tags.size > SpotTag::MAX_DISPLAY_NUMBER
   end
 
-  def difference_of_max(spot_tags:)
+  def difference_of_max(spot_tags)
     (spot_tags.size) - SpotTag::MAX_DISPLAY_NUMBER
   end
 end
