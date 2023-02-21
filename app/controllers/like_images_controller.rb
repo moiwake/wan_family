c9ea0d991848c6aa9008033fab039caf8d351d4d
moiwake <miho.kj.sp.195@gmail.com>
2023-02-21 15:40:00 +0900

    LikeImagesController：set_blobメソッドをDRYに

--- app/controllers/like_images_controller.rb
+++ app/controllers/like_images_controller.rb
@@ -1,21 +1,20 @@
 class LikeImagesController < ApplicationController
-  before_action :set_blob
-
   def create
     @like_image = current_user.like_images.create(image_id: params[:image_id], blob_id: params[:blob_id])
+    set_blob
     render "create_and_destroy"
   end
 
   def destroy
     @like_image = LikeImage.find(params[:id])
     @like_image.destroy
+    set_blob
     render "create_and_destroy"
   end
 
   private
 
   def set_blob
-    image = Image.find(params[:image_id])
-    @blob = OrderedImageBlobsQuery.call(parent_image: image).find(params[:blob_id])
+    @blob = @like_image.image.files.blobs.preload(attachments: :record).find(params[:blob_id])
   end
 end
