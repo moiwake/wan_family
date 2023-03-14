09ee5a21bf1b41ddf84de13823fdc7ea2d193e0b
moiwake <miho.kj.sp.195@gmail.com>
2023-03-14 22:50:23 +0900

    CreatedInSpecificPeriodQueryの作成：特定期間に作成したレコードを取得する

---
+++ app/queries/created_in_specific_period_query.rb
@@ -0,0 +1,19 @@
+class CreatedInSpecificPeriodQuery
+  attr_reader :scope, :date, :number
+
+  def initialize(scope:, date:, number:)
+    @scope = scope
+    @date = date
+    @number = number
+  end
+
+  def self.call(scope:, date:, number:)
+    new(scope: scope, date: date, number: number).for_specific_period
+  end
+
+  def for_specific_period
+    to = Time.current
+    from = (to - number.send(date)).at_beginning_of_day
+    scope.where(created_at: from...to)
+  end
+end
