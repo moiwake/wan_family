af92f55e9a369f4cc4bdd6eb545067631afa0813
moiwake <miho.kj.sp.195@gmail.com>
2023-03-26 17:38:20 +0900

    view/images/index：スポットの投稿画像一覧ページを実装

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,7 +1,8 @@
 class ImagesController < ApplicationController
+  before_action :set_spot, :set_favorite_spot, :set_tags_user_put_on_spot, only: :index
+
   def index
-    @spot = Spot.find(params[:spot_id])
-    @blobs = OrderedImageBlobsQuery.call(parent_record: @spot.images, order_params: params).preload(attachments: :record)
+    @blobs = ImageBlobs::OrderedQuery.call(parent_record: @spot.images, order_params: params).preload(attachments: :record)
   end
 
   def show
@@ -9,6 +10,24 @@ class ImagesController < ApplicationController
 
     if current_user.present?
       @image_like = ImageLike.find_by(user_id: current_user.id, blob_id: params[:blob_id])
+    end
+  end
+
+  private
+
+  def set_spot
+    @spot = Spot.find(params[:spot_id])
+  end
+
+  def set_favorite_spot
+    if current_user.present?
+      @favorite_spot = current_user.favorite_spots.find_by(spot_id: @spot.id)
+    end
+  end
+
+  def set_tags_user_put_on_spot
+    if current_user.present?
+      @tags_user_put_on_spot = SpotTag.get_tags_user_put_on_spot(user_id: current_user.id, spot_id: @spot.id)
     end
   end
 end
