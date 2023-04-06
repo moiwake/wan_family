4ab76276fde02ed3eb42d04c21da9657d2e5f6a6
moiwake <miho.kj.sp.195@gmail.com>
2023-04-06 16:44:34 +0900

    user_decorator_spec：display_dog_avatarメソッドの追加修正

--- spec/factories/users_factory_bot.rb
+++ spec/factories/users_factory_bot.rb
@@ -10,6 +10,7 @@ FactoryBot.define do
 
       after(:create) do |user|
         user.human_avatar.attach(io: File.open('spec/fixtures/images/test1.png'), filename: 'test1.png', content_type: 'image/png')
+        user.dog_avatar.attach(io: File.open('spec/fixtures/images/test2.png'), filename: 'test2.png', content_type: 'image/png')
       end
     end
   end
