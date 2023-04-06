53777dca6a9c8d5b044e6841b4723ec3bd2df99e
moiwake <miho.kj.sp.195@gmail.com>
2023-04-06 20:06:23 +0900

    spot_decorator_spec：get_dog_score_avgメソッド・get_human_score_avgメソッドのテスト作成、他：メソッド名修正

--- spec/decorators/spot_decorator_spec.rb
+++ spec/decorators/spot_decorator_spec.rb
@@ -1,34 +1,72 @@
 require 'rails_helper'
 
 RSpec.describe SpotDecorator, type: :decorator do
-  let(:spot) { create(:spot) }
+  let!(:spot) { create(:spot) }
 
-  describe "#find_attached_rules" do
+  describe "#get_attached_saved_rules" do
     let(:attached_rules) { create_list(:rule, 2, spot_id: spot.id, answer: "1") }
     let(:not_attached_rules) { create_list(:rule, 2, spot_id: spot.id, answer: "0") }
 
-    it "あるスポットに紐付いた、保存済みの同伴ルールのレコードうち、スポットに適用されると回答されたものを取得する"do
+    it "スポットに関連するRuleレコードのうち、スポットに適用されると回答されたレコード群のみを返す"do
       attached_rules.each do |attached_rule|
-        expect(spot.decorate.find_attached_rules.include?(attached_rule)).to eq(true)
+        expect(spot.decorate.get_attached_saved_rules.include?(attached_rule)).to eq(true)
       end
 
       not_attached_rules.each do |not_attached_rule|
-        expect(spot.decorate.find_attached_rules.include?(not_attached_rule)).to eq(false)
+        expect(spot.decorate.get_attached_saved_rules.include?(not_attached_rule)).to eq(false)
       end
     end
   end
 
-  describe "#get_checked_rule_opt" do
+  describe "#get_attached_unsaved_rules" do
     let(:attached_rules) { spot.rules.build([{ answer: "1" }, { answer: "1" }]) }
     let(:not_attached_rules) { spot.rules.build([{ answer: "0" }, { answer: "0" }]) }
 
-    it "あるスポットに紐付いた、保存前の同伴ルールのレコードうち、スポットに適用されると回答されたものを配列で取得する"do
+    it "スポットに関連するRuleレコードのうち、スポットに適用されると回答されたレコード群のみを返す"do
       attached_rules.each do |attached_rule|
-        expect(spot.decorate.get_checked_rule_opt.include?(attached_rule)).to eq(true)
+        expect(spot.decorate.get_attached_unsaved_rules.include?(attached_rule)).to eq(true)
       end
 
       not_attached_rules.each do |not_attached_rule|
-        expect(spot.decorate.get_checked_rule_opt.include?(not_attached_rule)).to eq(false)
+        expect(spot.decorate.get_attached_unsaved_rules.include?(not_attached_rule)).to eq(false)
+      end
+    end
+  end
+
+  describe "#get_dog_score_avg" do
+    context "スポットに関連するReviewレコードが存在するとき" do
+      let(:review_1) { create(:review, dog_score: 1, spot_id: spot.id) }
+      let(:review_2) { create(:review, dog_score: 3, spot_id: spot.id) }
+      let(:review_3) { create(:review, dog_score: 3, spot_id: spot.id) }
+      let!(:review_ary) { [review_1.dog_score, review_2.dog_score, review_3.dog_score] }
+
+      it "dog_scoreカラムの平均値を、小数点第一位で四捨五入した値を返す" do
+        expect(spot.decorate.get_dog_score_avg).to eq((review_ary.sum.to_f / review_ary.length).round(1))
+      end
+    end
+
+    context "スポットに関連するReviewレコードが存在しないとき" do
+      it "nilを返す" do
+        expect(spot.decorate.get_dog_score_avg).to eq(nil)
+      end
+    end
+  end
+
+  describe "#get_human_score_avg" do
+    context "スポットに関連するReviewレコードが存在するとき" do
+      let(:review_1) { create(:review, human_score: 1, spot_id: spot.id) }
+      let(:review_2) { create(:review, human_score: 3, spot_id: spot.id) }
+      let(:review_3) { create(:review, human_score: 3, spot_id: spot.id) }
+      let!(:review_ary) { [review_1.human_score, review_2.human_score, review_3.human_score] }
+
+      it "human_scoreカラムの平均値を、小数点第一位で四捨五入した値を返す" do
+        expect(spot.decorate.get_human_score_avg).to eq((review_ary.sum.to_f / review_ary.length).round(1))
+      end
+    end
+
+    context "スポットに関連するReviewレコードが存在しないとき" do
+      it "nilを返す" do
+        expect(spot.decorate.get_human_score_avg).to eq(nil)
       end
     end
   end
