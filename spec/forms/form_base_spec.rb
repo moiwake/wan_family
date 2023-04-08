02a85b3b09cdcde307e8cf7f51cd452dee5e426f
moiwake <miho.kj.sp.195@gmail.com>
2023-04-08 20:51:07 +0900

    form_base_spec：子クラスから呼び出す形のテストに修正、check_and_add_errorsメソッド・add_errorsメソッド・persistメソッド：FormBaseの変更に合わせて修正

--- spec/forms/form_base_spec.rb
+++ spec/forms/form_base_spec.rb
@@ -1,27 +1,119 @@
 require 'rails_helper'
 
 RSpec.describe FormBase, type: :model do
-  let!(:form_base_instance) { FormBase.new }
+  let(:child_class_instance) { ChildClass.new }
+
+  before(:all) do
+    class ChildClass < FormBase
+      attr_accessor :spot
+
+      def initialize(attributes: nil, spot: Spot.new)
+        @spot = spot
+        super(attributes: attributes, record: spot)
+      end
+    end
+  end
+
+  describe "#initialize" do
+    context "引数が渡されていないとき" do
+      it "変数attributesにdefault_attributesメソッドの返り値が代入される" do
+        expect(child_class_instance.attributes).to eq(child_class_instance.send(:default_attributes))
+      end
+    end
+  end
 
   describe "#invalid?" do
-    before do
-      allow(form_base_instance).to receive(:check_and_add_error)
-    end
+    before { allow(child_class_instance).to receive(:check_and_add_errors) }
 
     it "check_and_add_errorメソッドを呼び出す" do
-      form_base_instance.invalid?
-      expect(form_base_instance).to have_received(:check_and_add_error).once
+      child_class_instance.invalid?
+      expect(child_class_instance).to have_received(:check_and_add_errors).once
     end
   end
 
   describe "#save" do
-    before do
-      allow(form_base_instance).to receive(:persist)
-    end
+    before { allow(child_class_instance).to receive(:persist) }
 
     it "persistメソッドを呼び出す" do
-      form_base_instance.save
-      expect(form_base_instance).to have_received(:persist).once
+      child_class_instance.save
+      expect(child_class_instance).to have_received(:persist).once
+    end
+  end
+
+  describe "#check_and_add_errors" do
+    subject(:return_value) { child_class_instance.send(:check_and_add_errors) }
+
+    context "レコードの属性値が有効なとき" do
+      let!(:allowed_area) { create(:allowed_area) }
+      let!(:category) { create(:category) }
+      let!(:prefecture) { create(:prefecture) }
+
+      before do
+        child_class_instance.record.assign_attributes(
+          attributes_for(:spot, allowed_area_id: allowed_area.id, category_id: category.id, prefecture_id: prefecture.id)
+        )
+      end
+
+      it "falseを返す" do
+        expect(return_value).to eq(false)
+      end
+    end
+
+    context "レコードの属性値が不正なとき" do
+      before { allow(child_class_instance).to receive(:add_errors) }
+
+      it "add_errorsメソッドを呼び出す" do
+        subject
+        expect(child_class_instance).to have_received(:add_errors).once
+      end
+    end
+  end
+
+  describe "#add_errors" do
+    before do
+      child_class_instance.record.invalid?
+      child_class_instance.send(:add_errors)
+    end
+
+    it "レコードのエラーメッセージがerrorsコレクションに追加される" do
+      expect(child_class_instance.errors[:base]).to include("#{Spot.human_attribute_name(:name)}を入力してください")
+      expect(child_class_instance.errors[:base]).to include("#{Spot.human_attribute_name(:address)}を入力してください")
+      expect(child_class_instance.errors[:base]).to include("#{Spot.human_attribute_name(:category)}を入力してください")
+      expect(child_class_instance.errors[:base]).to include("#{Spot.human_attribute_name(:allowed_area)}を入力してください")
+    end
+  end
+
+  describe "#persist" do
+    subject(:return_value) { child_class_instance.send(:persist) }
+
+    context "check_and_add_errorsメソッドの返り値がfalseのとき" do
+      let!(:allowed_area) { create(:allowed_area) }
+      let!(:category) { create(:category) }
+      let!(:prefecture) { create(:prefecture) }
+
+      before do
+        child_class_instance.record.assign_attributes(
+          attributes_for(:spot, allowed_area_id: allowed_area.id, category_id: category.id, prefecture_id: prefecture.id)
+        )
+      end
+
+      it "レコードが保存される" do
+        expect { subject }.to change { Spot.count }.by(1)
+      end
+
+      it "trueを返す" do
+        expect(return_value).to eq(true)
+      end
+    end
+
+    context "check_and_add_errorsメソッドの返り値がtrueのとき" do
+      it "レコードは保存されない" do
+        expect { subject }.to change { Spot.count }.by(0)
+      end
+
+      it "falseを返す" do
+        expect(return_value).to eq(false)
+      end
     end
   end
 end
