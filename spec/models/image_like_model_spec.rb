102c02964f1774f89fab13dc1253042258239c47
moiwake <miho.kj.sp.195@gmail.com>
2023-04-11 22:44:37 +0900

    image_like_model_spec：再作成

---
+++ spec/models/image_like_model_spec.rb
@@ -0,0 +1,74 @@
+require "rails_helper"
+require 'support/shared_examples/model_spec'
+
+RSpec.describe ImageLike, type: :model do
+  let!(:image) { create(:image, :attached) }
+  let!(:blob) { image.files_blobs[0] }
+  let!(:image_like) { create(:image_like, image_id: image.id, blob_id: blob.id) }
+
+  context "全カラムのデータが有効なとき" do
+    let(:valid_object) { image_like }
+
+    it_behaves_like "the object is valid"
+  end
+
+  describe "presenceのバリデーション" do
+    let(:invalid_object) { build(:image_like, attribute => (type == :nil ? nil : "")) }
+    let(:message) { "を入力してください" }
+
+    context "user_idカラム" do
+      let(:attribute) { :user }
+
+      context "nilのとき" do
+        let(:type) { :nil }
+
+        it_behaves_like "adds validation error messages"
+      end
+    end
+
+    context "image_idカラム" do
+      let(:attribute) { :image }
+
+      context "nilのとき" do
+        let(:type) { :nil }
+
+        it_behaves_like "adds validation error messages"
+      end
+    end
+
+    context "blob_idカラム" do
+      let(:attribute) { :blob_id }
+
+      context "nilのとき" do
+        let(:type) { :nil }
+
+        it_behaves_like "adds validation error messages"
+      end
+    end
+  end
+
+  describe "uniquenessのバリデーション" do
+    let(:message) { "はすでに存在します" }
+
+    context "user_idカラムとblob_idカラムの組み合わせが重複しているとき" do
+      let(:attribute) { :user }
+      let(:invalid_object) { build(:image_like, user_id: image_like.user_id, blob_id: image_like.blob_id) }
+
+      it_behaves_like "adds validation error messages"
+    end
+
+    context "user_idカラムのみが重複しているとき" do
+      let!(:another_blob) { image.files_blobs[1] }
+      let(:valid_object) { build(:image_like, user_id: image_like.user_id, blob_id: another_blob.id) }
+
+      it_behaves_like "the object is valid"
+    end
+
+    context "blob_idカラムのみが重複しているとき" do
+      let!(:another_user) { create(:user) }
+      let(:valid_object) { build(:image_like, user_id: another_user.id, blob_id: image_like.blob_id) }
+
+      it_behaves_like "the object is valid"
+    end
+  end
+end
