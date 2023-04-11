b4f7daf9d05ec3736218dbcec3420f52db2465be
moiwake <miho.kj.sp.195@gmail.com>
2023-04-11 23:07:44 +0900

    prefecture_model_spec：バリデーションテストを作成

--- spec/models/prefecture_model_spec.rb
+++ spec/models/prefecture_model_spec.rb
@@ -1,10 +1,71 @@
 require "rails_helper"
-# require 'support/shared_examples/model_spec'
+require 'support/shared_examples/model_spec'
 
 RSpec.describe Prefecture, type: :model do
-  describe "scope #find_prefecture_name" do
-    it "地方名が引数の値と一致するデータの県名を、配列で取得する" do
-      expect(find_prefecture_name("東北")).to eq(["青森県", "岩手県", "宮城県", "秋田県", "山形県", "福島県"])
+  let!(:prefecture) { create(:prefecture) }
+
+  describe "presenceのバリデーション" do
+    let(:invalid_object) { build(:prefecture, attribute => (type == :nil ? nil : "")) }
+    let(:message) { "を入力してください" }
+
+    context "nameカラム" do
+      let(:attribute) { :name }
+
+      context "nilのとき" do
+        let(:type) { :nil }
+
+        it_behaves_like "adds validation error messages"
+      end
+
+      context "空文字のとき" do
+        let(:type) { :empty }
+
+        it_behaves_like "adds validation error messages"
+      end
+    end
+
+    context "name_romaカラム" do
+      let(:attribute) { :name }
+
+      context "nilのとき" do
+        let(:type) { :nil }
+
+        it_behaves_like "adds validation error messages"
+      end
+
+      context "空文字のとき" do
+        let(:type) { :empty }
+
+        it_behaves_like "adds validation error messages"
+      end
+    end
+
+    context "regionカラム" do
+      let(:attribute) { :region }
+
+      context "nilのとき" do
+        let(:type) { :nil }
+
+        it_behaves_like "adds validation error messages"
+      end
+    end
+  end
+
+  describe "uniquenessのバリデーション" do
+    let(:message) { "はすでに存在します" }
+
+    context "nameカラムが重複しているとき" do
+      let(:attribute) { :name }
+      let(:invalid_object) { build(:prefecture, name: prefecture.name) }
+
+      it_behaves_like "adds validation error messages"
+    end
+
+    context "name_romaカラムが重複しているとき" do
+      let(:attribute) { :name_roma }
+      let(:invalid_object) { build(:prefecture, name_roma: prefecture.name_roma) }
+
+      it_behaves_like "adds validation error messages"
     end
   end
 end
