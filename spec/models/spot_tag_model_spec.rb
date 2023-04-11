efae47ea6ae226a931de33315f555006978b0cad
moiwake <miho.kj.sp.195@gmail.com>
2023-04-12 00:25:15 +0900

    spot_tag_model_spec：scopeのテストを作成

--- spec/models/spot_tag_model_spec.rb
+++ spec/models/spot_tag_model_spec.rb
@@ -50,4 +50,32 @@ RSpec.describe SpotTag, type: :model do
       end
     end
   end
+
+  describe "scope" do
+    let!(:user) { create(:user) }
+    let!(:anothre_user) { create(:user) }
+
+    describe "scope#get_tag_names_user_created" do
+      before do
+        create_list(:spot_tag, 2, user: user, name: "tag_name_1")
+        create(:spot_tag, user: user, name: "tag_name_2")
+        create(:spot_tag, user: anothre_user)
+      end
+
+      it "更新日、でなければ作成日の降順に並び替えした、引数のuser_idを持つSpotTagレコードの、重複を除いたnameカラムの配列を返す" do
+        expect(SpotTag.get_tag_names_user_created(user_id: user.id)).to eq(["tag_name_2", "tag_name_1"])
+      end
+    end
+
+    describe "scope#get_tags_user_put_on_spot" do
+      let!(:spot) { create(:spot) }
+      let(:spot_tags) { SpotTag.where(user_id: user.id, spot_id: spot.id).order(updated_at: :desc, created_at: :desc) }
+
+      before { create_list(:spot_tag, 2, user: user, spot: spot) }
+
+      it "更新日、でなければ作成日の降順に並び替えした、引数のuser_idとspot_idを持つSpotTagレコード群を返す" do
+        expect(SpotTag.get_tags_user_put_on_spot(user_id: user.id, spot_id: spot.id)).to eq(spot_tags)
+      end
+    end
+  end
 end
