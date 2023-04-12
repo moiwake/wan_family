aee1c2ba8b75b9b96dd77ccea13915fcf6f23790
moiwake <miho.kj.sp.195@gmail.com>
2023-04-12 15:23:05 +0900

    spots・reviews・image_blobs/ranked_query：callメソッド：引数にscope、parent_recordを追加

--- app/queries/reviews/ranked_query.rb
+++ app/queries/reviews/ranked_query.rb
@@ -1,7 +1,7 @@
 module Reviews
   class RankedQuery
-    def self.call(rank_num: 10)
-      Reviews::OrderedQuery.call(order_params: { by: "likes_count" }).limit(rank_num)
+    def self.call(scope: nil, parent_record: nil, rank_num: 10)
+      Reviews::OrderedQuery.call(scope: scope, parent_record: parent_record, order_params: { by: "likes_count" }).limit(rank_num)
     end
   end
 end
--- app/queries/spots/ranked_query.rb
+++ app/queries/spots/ranked_query.rb
@@ -1,7 +1,7 @@
 module Spots
   class RankedQuery
-    def self.call(rank_num: 10)
-      Spots::OrderedQuery.call(order_params: { by: "likes_count" }).limit(rank_num)
+    def self.call(scope: nil, parent_record: nil, rank_num: 10)
+      Spots::OrderedQuery.call(scope: scope, parent_record: parent_record, order_params: { by: "likes_count" }).limit(rank_num)
     end
   end
 end
