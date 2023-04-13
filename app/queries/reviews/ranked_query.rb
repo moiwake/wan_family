445897bd0a1a9966b7143fb2fbdff87898fd03c8
moiwake <miho.kj.sp.195@gmail.com>
2023-04-13 19:02:26 +0900

    RankedQuery：limit_scopeメソッドの作成：レコード数を限定する処理をcallメソッドから独立させる、RANK_NUMBERの定数を設定

--- app/queries/reviews/ranked_query.rb
+++ app/queries/reviews/ranked_query.rb
@@ -1,7 +1,15 @@
 module Reviews
   class RankedQuery
-    def self.call(scope: nil, parent_record: nil, rank_num: 10)
-      Reviews::OrderedQuery.call(scope: scope, parent_record: parent_record, order_params: { by: "likes_count" }).limit(rank_num)
+    RANK_NUMBER = 10
+
+    class << self
+      def call(scope: nil, parent_record: nil, rank_num: RANK_NUMBER)
+        limit_scope(Reviews::OrderedQuery.call(scope: scope, parent_record: parent_record, order_params: { by: "likes_count" }), rank_num)
+      end
+
+      def limit_scope(scope, rank_num)
+        scope.limit(rank_num)
+      end
     end
   end
 end
