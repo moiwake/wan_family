a68edca7b0c8e3d9ce7076968c4365bce4940c00
moiwake <miho.kj.sp.195@gmail.com>
2023-04-14 16:22:06 +0900

    image_likes_controller：set_image_blobメソッドをbefore_actionで記述、image_likes_request_spec：モデル名を修正して再作成

--- app/controllers/image_likes_controller.rb
+++ app/controllers/image_likes_controller.rb
@@ -1,20 +1,21 @@
 class ImageLikesController < ApplicationController
+  before_action :set_image_blob
+
   def create
-    @image_like = current_user.image_likes.create(image_id: params[:image_id], image_blob_id: params[:image_blob_id])
-    set_image_blob
+    @image_like = @image.image_likes.create(user: current_user, blob_id: params[:image_blob_id])
     render "create_and_destroy"
   end
 
   def destroy
     @image_like = ImageLike.find(params[:id])
     @image_like.destroy
-    set_image_blob
     render "create_and_destroy"
   end
 
   private
 
   def set_image_blob
-    @image_blob = @image_like.image.files_blobs.preload(attachments: :record).find(params[:image_blob_id])
+    @image = Image.find(params[:image_id])
+    @image_blob = @image.files_blobs.preload(:attachments).find(params[:image_blob_id])
   end
 end
---
+++ spec/requests/image_likes_request_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+require 'support/shared_examples/request_spec'
+
+RSpec.describe "ImageLikes", type: :request do
+  let!(:user) { create(:user) }
+  let!(:spot) { create(:spot) }
+  let!(:image) { create(:image, :attached) }
+  let!(:image_blob) { image.files_blobs[0] }
+
+  before { sign_in user }
+
+  describe "POST /create" do
+    let(:new_like_image) { ImageLike.last }
+
+    subject { post spot_image_image_likes_path(spot, image), params: { image_blob_id: image_blob.id }, xhr: true }
+
+    it "画像にいいねを登録できる" do
+      expect { subject }.to change { ImageLike.count }.by(1)
+      expect(new_like_image.user_id).to eq(user.id)
+      expect(new_like_image.image_id).to eq(image.id)
+      expect(new_like_image.blob_id).to eq(image_blob.id)
+    end
+
+    it_behaves_like "returns http success"
+  end
+
+  describe "DELETE /destroy" do
+    let!(:image_like) { create(:image_like, user: user, image: image, blob_id: image_blob.id) }
+
+    subject { delete spot_image_image_like_path(spot, image, image_like), params: { image_blob_id: image_blob.id }, xhr: true }
+
+    it "画像のいいねを削除できる" do
+      expect { subject }.to change { ImageLike.count }.by(-1)
+    end
+
+    it_behaves_like "returns http success"
+  end
+end
