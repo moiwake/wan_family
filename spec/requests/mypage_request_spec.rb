e6f74be7ab73f2e29042a9478e863ec552e47022
moiwake <miho.kj.sp.195@gmail.com>
2023-04-18 17:38:14 +0900

    mypage_request_spec：ファイル名変更（旧users_request_spec）、コントローラーに追加したアクションにテストを追加

--- spec/requests/mypage_request_spec.rb
+++ spec/requests/mypage_request_spec.rb
@@ -1,21 +1,38 @@
 require 'rails_helper'
 require 'support/shared_examples/request_spec'
 
-RSpec.describe "Users", type: :request do
+RSpec.describe "Mypage", type: :request do
   let!(:user) { create(:user, :updated_profile_user) }
 
-  describe "GET /index" do
+  describe "GET /favorite_spot_index" do
     context "ログインしているとき" do
       before do
         sign_in user
-        get mypage_path
+        get users_mypage_favorite_spot_index_path
       end
 
       it_behaves_like "returns http success"
     end
 
     context "ログインしていないとき" do
-      before { get mypage_path }
+      before { get users_mypage_favorite_spot_index_path }
+
+      it_behaves_like "redirects to login page"
+    end
+  end
+
+  describe "GET /spot_tag_index" do
+    context "ログインしているとき" do
+      before do
+        sign_in user
+        get users_mypage_spot_tag_index_path
+      end
+
+      it_behaves_like "returns http success"
+    end
+
+    context "ログインしていないとき" do
+      before { get users_mypage_spot_tag_index_path }
 
       it_behaves_like "redirects to login page"
     end
@@ -25,14 +42,14 @@ RSpec.describe "Users", type: :request do
     context "ログインしているとき" do
       before do
         sign_in user
-        get users_spot_index_path
+        get users_mypage_spot_index_path
       end
 
       it_behaves_like "returns http success"
     end
 
     context "ログインしていないとき" do
-      before { get users_spot_index_path }
+      before { get users_mypage_spot_index_path }
 
       it_behaves_like "redirects to login page"
     end
@@ -42,14 +59,14 @@ RSpec.describe "Users", type: :request do
     context "ログインしているとき" do
       before do
         sign_in user
-        get users_review_index_path
+        get users_mypage_review_index_path
       end
 
       it_behaves_like "returns http success"
     end
 
     context "ログインしていないとき" do
-      before { get users_review_index_path }
+      before { get users_mypage_review_index_path }
 
       it_behaves_like "redirects to login page"
     end
@@ -59,14 +76,14 @@ RSpec.describe "Users", type: :request do
     context "ログインしているとき" do
       before do
         sign_in user
-        get users_image_index_path
+        get users_mypage_image_index_path
       end
 
       it_behaves_like "returns http success"
     end
 
     context "ログインしていないとき" do
-      before { get users_image_index_path }
+      before { get users_mypage_image_index_path }
 
       it_behaves_like "redirects to login page"
     end
@@ -76,37 +93,40 @@ RSpec.describe "Users", type: :request do
     context "ログインしているとき" do
       before do
         sign_in user
-        get edit_profile_path
+        get users_mypage_profile_edit_path
       end
 
       it_behaves_like "returns http success"
     end
 
     context "ログインしていないとき" do
-      before { get edit_profile_path }
+      before { get users_mypage_profile_edit_path }
 
       it_behaves_like "redirects to login page"
     end
   end
 
   describe "PATCH /update" do
-    let(:updated_params) { { introduction: "updated introduction", human_avatar: updated_human_avatar } }
-    let(:updated_human_avatar) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'test2.png'), 'image/png') }
+    let(:updated_params) { { introduction: "updated introduction", human_avatar: updated_human_avatar, dog_avatar: updated_dog_avatar } }
+    let(:updated_human_avatar) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'test3.png'), 'image/png') }
+    let(:updated_dog_avatar) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'images', 'test4.png'), 'image/png') }
     let(:updated_human_avatar_filename) { updated_human_avatar.original_filename }
+    let(:updated_dog_avatar_filename) { updated_dog_avatar.original_filename }
 
-    subject { patch profile_path, params: { user: updated_params } }
+    subject { patch users_mypage_profile_path, params: { user: updated_params } }
 
     before { sign_in user }
 
-    it "ログインユーザーのレコードを更新できる" do
+    it "ログインユーザーのUserレコードを更新できる" do
       expect { subject }.to change { User.count }.by(0)
       expect(user.introduction).to eq(updated_params[:introduction])
       expect(user.human_avatar.filename.to_s).to eq(updated_human_avatar_filename)
+      expect(user.dog_avatar.filename.to_s).to eq(updated_dog_avatar_filename)
     end
 
     it "更新後にリダイレクトする" do
       subject
-      expect(response).to redirect_to(edit_profile_path)
+      expect(response).to redirect_to(users_mypage_favorite_spot_index_path)
     end
   end
 end
