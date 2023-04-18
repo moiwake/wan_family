8aaf7c9cc30b44b58f499ecdd5c8f8aab0b18287
moiwake <miho.kj.sp.195@gmail.com>
2023-04-18 16:56:59 +0900

    top_controller：application_controllerにset_qメソッドを記述していることで不要になった処理を削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,6 @@
 class TopController < ApplicationController
   def index
     session.delete(:q)
-    @q = Spot.ransack(params[:q])
     @ranked_spots = Spots::RankedQuery.call.eager_load(:category, :prefecture).preload(:images)
     @weekly_ranked_spots = Spots::WeeklyRankedQuery.call.preload(:images)
     @weekly_ranked_image_blobs = ImageBlobs::WeeklyRankedQuery.call.preload(attachments: :record)
@@ -13,18 +12,10 @@ class TopController < ApplicationController
   end
 
   def word_search
-    if params[:q]
-      @q = Spot.ransack({ combinator: "and", groupings: set_search_groupings("and") })
-    elsif session[:q]
-      @q = Spot.ransack(session[:q])
-    end
-
+    @q = Spot.ransack({ combinator: "and", groupings: set_search_groupings("and") })
     @results = Spots::OrderedQuery.call(scope: @q.result(distinct: true), order_params: params).page(params[:page]).eager_load(:category).preload(:images)
-
-    if @q.present? && params[:q]
-      @q = Spot.ransack(params[:q]["and"])
-      session[:q] = params[:q]["and"]
-    end
+    @q = Spot.ransack(params[:q]["and"])
+    session[:q] = params[:q]["and"]
   end
 
   private
@@ -34,7 +25,7 @@ class TopController < ApplicationController
     grouping_ary = []
 
     search_params.keys.each do |key|
-      if search_params["name_or_address_cont"]
+      if key == "name_or_address_cont"
         keywords = search_params[key].split(/[\p{blank}\s]+/)
         keywords.reduce(grouping_ary){ |ary, word| ary.push({ key => word }) }
       else
