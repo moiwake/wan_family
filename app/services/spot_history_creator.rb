94dd1021a8ef903c0e6a456892963e978bf77e44
moiwake <miho.kj.sp.195@gmail.com>
2023-04-18 18:07:42 +0900

    SpotHistoryCreator：インスタンス変数の定義をinitializeメソッドで行うよう修正

--- app/services/spot_history_creator.rb
+++ app/services/spot_history_creator.rb
@@ -1,15 +1,17 @@
 class SpotHistoryCreator
-  class << self
-    def call(spot: nil, user: nil, history: nil)
-      spot = spot
-      user = user
-      history = history
+  attr_reader :spot, :user, :history
 
-      create_spot_histories(spot, user, history)
-    end
+  def initialize(spot:, user:, history:)
+    @spot = spot
+    @user = user
+    @history = history
+  end
 
-    def create_spot_histories(spot, user, history)
-      spot.spot_histories.create(user_id: user.id, history: history)
-    end
+  def self.call(spot: nil, user: nil, history: nil)
+    new(spot: spot, user: user, history: history).create_spot_histories
+  end
+
+  def create_spot_histories
+    spot.spot_histories.create(user_id: user.id, history: history)
   end
 end
