9a70513a90990e2d804930488d0e9ac4db46bb07
moiwake <miho.kj.sp.195@gmail.com>
2023-04-18 17:41:14 +0900

    devise_redirect_spec：フレンドリーフォワーディング機能を追加したことによる修正

--- spec/requests/concerns/devise_redirect_spec.rb
+++ spec/requests/concerns/devise_redirect_spec.rb
@@ -3,56 +3,73 @@ require 'rails_helper'
 class DummyController < DeviseController
   include DeviseRedirect
 
-  attr_accessor :resource
-  def initialize(resource: nil)
+  attr_reader :resource, :scope_name
+
+  def initialize(resource: nil, scope_name: nil)
     @resource = resource
+    @scope_name = scope_name
   end
 end
 
 RSpec.describe "DeviseRedirect", :type => :request do
   let(:admin) { create(:admin) }
-  let(:admin_controller) { DummyController.new(resource: admin) }
   let(:other) { double("other") }
-  let(:other_controller) { DummyController.new(resource: other) }
 
   before do
     def return_path(path)
       Rails.application.routes.url_helpers.send(path)
     end
 
-    allow(admin_controller).to receive(:rails_admin_path) do
+    allow(controller).to receive(:rails_admin_path) do
       return_path("rails_admin_path")
     end
 
-    allow(other_controller).to receive(:root_path) do
+    allow(controller).to receive(:root_path) do
       return_path("root_path")
     end
   end
 
   describe "#after_sign_in_path_for" do
+    subject(:return_value) { controller.after_sign_in_path_for(controller.resource) }
+
     context "引数のオブジェクトがAdminクラスのインスタンスのとき" do
+      let(:controller) { DummyController.new(resource: admin) }
+
       it "rails_admin_pathを返す" do
-        expect(admin_controller.after_sign_in_path_for(admin_controller.resource)).to eq(rails_admin_path)
+        expect(return_value).to eq(rails_admin_path)
       end
     end
 
     context "引数のオブジェクトがAdminクラスのインスタンスではないとき" do
-      it "root_pathを返す" do
-        expect(other_controller.after_sign_in_path_for(other_controller.resource)).to eq(root_path)
+      let(:controller) { DummyController.new(resource: other) }
+
+      before do
+        allow(controller).to receive(:stored_location_for).and_return("stored_location_path")
+        subject
+      end
+
+      it "引数を渡して、stored_location_forメソッドを呼び出す" do
+        expect(controller).to have_received(:stored_location_for).once.with(other)
       end
     end
   end
 
   describe "#after_sign_out_path_for" do
+    subject(:return_value) { controller.after_sign_out_path_for(controller.scope_name) }
+
     context "引数が':admin'のとき" do
+      let(:controller) { DummyController.new(scope_name: :admin) }
+
       it "rails_admin_pathを返す" do
-        expect(admin_controller.after_sign_in_path_for(admin_controller.resource)).to eq(rails_admin_path)
+        expect(return_value).to eq(rails_admin_path)
       end
     end
 
     context "引数が':admin'ではないとき" do
+      let(:controller) { DummyController.new(scope_name: :other) }
+
       it "root_pathを返す" do
-        expect(other_controller.after_sign_in_path_for(other_controller.resource)).to eq(root_path)
+        expect(return_value).to eq(root_path)
       end
     end
   end
