5a20b918d60fcba0ec01028814cef96f8f156bf9
moiwake <miho.kj.sp.195@gmail.com>
2023-04-29 16:48:19 +0900

    review_helpfulness_helper：review_helpfulness_btn_pathメソッドの作成：link_toタグのパスを返す：レビューへの役立った登録ボタンのリンクの場合分けをヘルパーに移行

--- app/helpers/review_helpfulness_helper.rb
+++ app/helpers/review_helpfulness_helper.rb
@@ -12,4 +12,18 @@ module ReviewHelpfulnessHelper
       ReviewHelpfulness.find_by(user_id: current_user.id, review_id: review.id)
     end
   end
+
+  def review_helpfulness_btn_path(review, review_helpfulness)
+    if current_user.present?
+      if review_posted_by_another?(review)
+        if review_helpful?(review_helpfulness)
+          { href: spot_review_review_helpfulness_path(review.spot, review, review_helpfulness), method: :delete, class: "review-helpfulness-remove-btn", remote: true }
+        else
+          { href: spot_review_review_helpfulnesses_path(review.spot, review), method: :post, class: "review-helpfulness-add-btn", remote: true }
+        end
+      end
+    else
+      { href: new_user_session_path, class: "sign-in-btn" }
+    end
+  end
 end
--- spec/helpers/review_helpfulnesses_helper_spec.rb
+++ spec/helpers/review_helpfulnesses_helper_spec.rb
@@ -67,4 +67,61 @@ RSpec.describe ReviewHelpfulnessHelper, type: :helper do
       end
     end
   end
+
+  describe "#review_helpfulness_btn_path" do
+    let!(:spot) { create(:spot) }
+
+    context "ログインユーザーが存在するとき" do
+      before { allow(helper).to receive(:current_user).and_return(user) }
+
+      context "引数のReviewレコードをログインユーザー以外が作成していたとき" do
+        let!(:another_user) { create(:user) }
+        let!(:review_by_another) { create(:review, user: another_user, spot: spot) }
+
+        context "引数のReviewHelpnessレコードが存在して、かつDBに保存済みであるとき" do
+          let(:review_helpfulness) { create(:review_helpfulness, user: user, review: review_by_another) }
+          let(:expected_hash) { { href: spot_review_review_helpfulness_path(spot, review_by_another, review_helpfulness), method: :delete, class: "review-helpfulness-remove-btn", remote: true } }
+
+          it "指定のハッシュを返す" do
+            expect(helper.review_helpfulness_btn_path(review_by_another, review_helpfulness)).to eq(expected_hash)
+          end
+        end
+
+        context "引数のReviewHelpnessレコードが存在しないとき" do
+          let(:expected_hash) { { href: spot_review_review_helpfulnesses_path(spot, review_by_another), method: :post, class: "review-helpfulness-add-btn", remote: true } }
+
+          it "指定のハッシュを返す" do
+            expect(helper.review_helpfulness_btn_path(review_by_another, nil)).to eq(expected_hash)
+          end
+        end
+
+        context "引数のReviewHelpnessレコードがDBされていないとき" do
+          let(:review_helpfulness) { create(:review_helpfulness, user: user, review: review_by_another) }
+          let(:expected_hash) { { href: spot_review_review_helpfulnesses_path(spot, review_by_another), method: :post, class: "review-helpfulness-add-btn", remote: true } }
+
+          before { review_helpfulness.destroy }
+
+          it "指定のハッシュを返す" do
+            expect(helper.review_helpfulness_btn_path(review_by_another, review_helpfulness)).to eq(expected_hash)
+          end
+        end
+      end
+
+      context "引数のReviewレコードをログインユーザー以外が作成していたとき" do
+        it "nilを返す" do
+          expect(helper.review_helpfulness_btn_path(review, nil)).to eq(nil)
+        end
+      end
+    end
+
+    context "ログインユーザーが存在しないとき" do
+      let(:expected_hash) { { href: new_user_session_path, class: "sign-in-btn" } }
+
+      before { allow(helper).to receive(:current_user).and_return(nil) }
+
+      it "指定のハッシュを返す" do
+        expect(helper.review_helpfulness_btn_path(nil, nil)).to eq(expected_hash)
+      end
+    end
+  end
 end
