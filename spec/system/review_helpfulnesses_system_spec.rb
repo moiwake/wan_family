635c9f553c31aa7c4fd476b928b1d2287d34a20b
moiwake <miho.kj.sp.195@gmail.com>
2023-04-29 16:48:54 +0900

    review_helpfulnesses_system_spec：viewの変更に伴う修正

--- spec/system/review_helpfulnesses_system_spec.rb
+++ spec/system/review_helpfulnesses_system_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe "ReviewHelpfulnessesSystemSpecs", type: :system, js: true do
   let!(:user) { create(:user) }
   let!(:spot) { create(:spot) }
-  let!(:review) { create(:review, spot_id: spot.id) }
-  let!(:review_helpfulnesses) { create_list(:review_helpfulness, 3, review_id: review.id) }
-  let!(:current_like_count) { ReviewHelpfulness.where(review_id: review.id).count }
+  let!(:review) { create(:review, spot: spot) }
+  let!(:review_helpfulnesses) { create_list(:review_helpfulness, 3, review: review) }
+  let!(:current_like_count) { review.review_helpfulnesses.count }
 
   describe "レビューのGood登録" do
-    before { visit spot_review_path(spot, review) }
+    before { visit spot_reviews_path(spot) }
 
     it "レビューに登録されているGoodの総計を表示する" do
-      expect(find("#post-review-helpfulness")).to have_content(current_like_count)
+      expect(page.all(".review-helpfulness-btn-wrap")[0]).to have_content(current_like_count)
     end
 
     context "ログインしているとき" do
@@ -21,12 +21,12 @@ RSpec.describe "ReviewHelpfulnessesSystemSpecs", type: :system, js: true do
         let!(:before_like_count) { ReviewHelpfulness.where(review_id: review.id).count }
         let(:new_like) { ReviewHelpfulness.last }
 
-        before { visit spot_review_path(spot, review) }
+        before { visit spot_reviews_path(spot) }
 
         it "レビューにGoodの登録ができる" do
           expect do
-            find("#add-review-helpfulness").click
-            expect(find("#post-review-helpfulness")).to have_content(before_like_count + 1)
+            find(".review-helpfulness-add-btn").click
+            expect(all(".review-helpfulness-btn-wrap")[0]).to have_content(before_like_count + 1)
           end.to change { ReviewHelpfulness.count }.by(1)
 
           expect(new_like.user_id).to eq(user.id)
@@ -35,15 +35,15 @@ RSpec.describe "ReviewHelpfulnessesSystemSpecs", type: :system, js: true do
       end
 
       context "ログインユーザーがレビューにGoodを登録しているとき" do
-        let!(:review_helpfulness) { create(:review_helpfulness, user_id: user.id, review_id: review.id) }
+        let!(:review_helpfulness) { create(:review_helpfulness, user: user, review: review) }
         let!(:before_like_count) { ReviewHelpfulness.where(review_id: review.id).count }
 
-        before { visit spot_review_path(spot, review) }
+        before { visit spot_reviews_path(spot) }
 
         it "レビューのGoodの登録を削除できる" do
           expect do
-            find("#remove-review-helpfulness").click
-            expect(find("#post-review-helpfulness")).to have_content(before_like_count - 1)
+            find(".review-helpfulness-remove-btn").click
+            expect(all(".review-helpfulness-btn-wrap")[0]).to have_content(before_like_count - 1)
           end.to change { ReviewHelpfulness.count }.by(-1)
         end
       end
@@ -53,18 +53,18 @@ RSpec.describe "ReviewHelpfulnessesSystemSpecs", type: :system, js: true do
 
         before do
           sign_in review_poster
-          visit spot_review_path(spot, review)
+          visit spot_reviews_path(spot)
         end
 
         it "Good登録のリンクが表示されない" do
-          expect(find("#post-review-helpfulness")).not_to have_selector("a")
+          expect(find(".review-helpfulness-btn-wrap")).to have_link(href: "")
         end
       end
     end
 
     context "ログインしていないとき" do
       it "ログインページへのリンクが表示される" do
-        expect(find("#post-review-helpfulness")).to have_link(href: new_user_session_path)
+        expect(find(".review-helpfulness-btn-wrap")).to have_link(href: new_user_session_path)
       end
     end
   end
