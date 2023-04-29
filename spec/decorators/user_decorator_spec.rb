b349dad4dcc48792c6877b1366f0d86f1a3527e6
moiwake <miho.kj.sp.195@gmail.com>
2023-04-29 11:51:56 +0900

    user_decorator_spec：total_of_images_posted_by_userメソッドのテスト作成

--- spec/decorators/user_decorator_spec.rb
+++ spec/decorators/user_decorator_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UserDecorator, type: :decorator do
-  let!(:user_with_avatar) { create(:user, :updated_profile_user) }
-  let!(:user_without_avatar) { create(:user) }
-
   describe "#display_human_avatar" do
+    let!(:user_with_avatar) { create(:user, :updated_profile_user) }
+    let!(:user_without_avatar) { create(:user) }
+
     context "ユーザー画像が登録されているとき" do
       it "登録されたユーザー画像が表示される" do
         expect(user_with_avatar.decorate.display_human_avatar).to eq(user_with_avatar.human_avatar)
@@ -19,6 +19,9 @@ RSpec.describe UserDecorator, type: :decorator do
   end
 
   describe "#display_dog_avatar" do
+    let!(:user_with_avatar) { create(:user, :updated_profile_user) }
+    let!(:user_without_avatar) { create(:user) }
+
     context "ユーザー画像が登録されているとき" do
       it "登録されたユーザー画像が表示される" do
         expect(user_with_avatar.decorate.display_dog_avatar).to eq(user_with_avatar.dog_avatar)
@@ -29,6 +32,16 @@ RSpec.describe UserDecorator, type: :decorator do
       it "未登録用の画像が表示される" do
         expect(user_without_avatar.decorate.display_dog_avatar).to eq("noavatar-dog.png")
       end
+    end
+  end
+
+  describe "#total_of_images_posted_by_user" do
+    let!(:user) { create(:user) }
+
+    before { create(:image, :attached, user: user) }
+
+    it "レシーバーのユーザーが登録したImageレコードに紐づくBlobレコードの数を返す" do
+      expect(user.decorate.total_of_images_posted_by_user).to eq(2)
     end
   end
 end
