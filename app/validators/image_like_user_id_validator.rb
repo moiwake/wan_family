c06f9334c905d793bd61543ac2e0e53e24ff11c5
moiwake <miho.kj.sp.195@gmail.com>
2023-05-01 15:05:50 +0900

    ReviewHelpfulnessUserIdValidator・ImageLikeUserIdValidator：外部キーがnilの場合のエラーを解消

--- app/validators/image_like_user_id_validator.rb
+++ app/validators/image_like_user_id_validator.rb
@@ -1,7 +1,9 @@
 class ImageLikeUserIdValidator < ActiveModel::Validator
   def validate(record)
-    if record.user_id == Image.find(record.image_id).user_id
-      record.errors.add :user, "投稿者が自分の画像に「いいね」を登録することはできません。"
+    if record.image_id
+      if record.user_id == Image.find(record.image_id).user_id
+        record.errors.add :user, "投稿者が自分の画像に「いいね」を登録することはできません。"
+      end
     end
   end
 end
--- app/validators/review_helpfulness_user_id_validator.rb
+++ app/validators/review_helpfulness_user_id_validator.rb
@@ -1,7 +1,9 @@
 class ReviewHelpfulnessUserIdValidator < ActiveModel::Validator
   def validate(record)
-    if record.user_id == Review.find(record.review_id).user_id
-      record.errors.add :user, "投稿者が自分のレビューに「役立った」を登録することはできません。"
+    if record.review_id
+      if record.user_id == Review.find(record.review_id).user_id
+        record.errors.add :user, "投稿者が自分のレビューに「役立った」を登録することはできません。"
+      end
     end
   end
 end
