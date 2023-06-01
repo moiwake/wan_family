936a64c2baff98c930159e58f442676a152eaa3a
moiwake <miho.kj.sp.195@gmail.com>
2023-06-01 22:30:53 +0900

    review_decorateor：get_files_ordered_by_blob_creation_dateメソッド：メソッド名をわかりやすく修正

--- app/decorators/review_decorator.rb
+++ app/decorators/review_decorator.rb
@@ -1,7 +1,7 @@
 class ReviewDecorator < Draper::Decorator
   delegate_all
 
-  def get_files_for_edit
+  def get_files_ordered_by_blob_creation_date
     if object.persisted? && object.reload.image.present?
       object.image.files.eager_load(:blob).order("blob.created_at desc, blob.id desc")
     else
--- spec/decorators/review_decorator_spec.rb
+++ spec/decorators/review_decorator_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ReviewDecorator, type: :decorator do
-  describe "#get_files_for_edit" do
+  describe "#get_files_ordered_by_blob_creation_date" do
     context "レシーバーがDBに保存されている、かつリロードしたレシーバーに関連するImageレコードが存在するとき" do
       let!(:review) { create(:review) }
-      let!(:image) { create(:image, :attached, review_id: review.id) }
-      let!(:ordered_files) { image.files.eager_load(:blob).order("blob.created_at desc, blob.id desc") }
+      let!(:image) { create(:image, :attached, review: review) }
+      let!(:ordered_file_ids) { [image.files[1].id, image.files[0].id] }
 
       it "Blobレコードの作成日順、同じ場合はid順に並んだFileレコードを返す" do
-        expect(review.decorate.get_files_for_edit).to eq(ordered_files)
+        expect(review.decorate.get_files_ordered_by_blob_creation_date.ids).to eq(ordered_file_ids)
       end
     end
 
@@ -17,7 +17,7 @@ RSpec.describe ReviewDecorator, type: :decorator do
       let!(:image) { build(:image, :attached, review_id: review.id) }
 
       it "空の配列を返す" do
-        expect(review.decorate.get_files_for_edit).to eq([])
+        expect(review.decorate.get_files_ordered_by_blob_creation_date).to eq([])
       end
     end
 
@@ -25,7 +25,7 @@ RSpec.describe ReviewDecorator, type: :decorator do
       let!(:review) { build(:review) }
 
       it "空の配列を返す" do
-        expect(review.decorate.get_files_for_edit).to eq([])
+        expect(review.decorate.get_files_ordered_by_blob_creation_date).to eq([])
       end
     end
   end
