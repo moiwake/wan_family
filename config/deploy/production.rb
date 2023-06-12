6290cb3c2320a6e3c295cd8479a48361fce204c0
moiwake <miho.kj.sp.195@gmail.com>
2023-06-13 07:42:47 +0900

    aws：SSL化：テストデプロイ

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,8 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-server 'wan-family-app.com', user: 'ec2-user', roles: %w(app db web)
+server '18.179.228.204', user: 'ec2-user', roles: %w(app db web)
+
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
